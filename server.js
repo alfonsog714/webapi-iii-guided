30b3558bd64e181972edfc919e7745d1f2da2fe0
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-14 20:29:37 -0700

    Added logger custom middleware

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+// middleware
+function logger(req, res, next) {
+  console.log(req.path);
+}
+
 server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
