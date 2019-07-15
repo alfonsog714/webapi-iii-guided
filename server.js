b1749e92857d9a6ec624da25bbec84e9cfa4a660
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-14 20:43:31 -0700

    added helmet and two new endpoints in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
 const hubsRouter = require("./hubs/hubs-router.js");
 
@@ -6,10 +7,21 @@ const server = express();
 
 // middleware
 function logger(req, res, next) {
-  console.log(req.path);
+  console.log(`${req.method} to ${req.path}`);
+  next();
 }
 
+server.use(logger);
+server.use(helmet());
 server.use(express.json());
+
+server.get("/free", (req, res) => {
+  res.status(200).json({ welcome: "Web 19 Developers!" });
+});
+
+server.get("/paid", (req, res) => {
+  res.status(200).json({ you: "Shall not pass!" });
+});
 
 server.use("/api/hubs", hubsRouter);
 
